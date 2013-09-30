5ff0475219ebf2a7eeed7cee040636917a04a3dd
Deborah <deborahc@ubuntu.ubuntu-domain>
2013-09-30 00:09:54 -0400

    Editing gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeborahcProj2::Application.config.secret_key_base = '1d6fcb3b52909b4bfd838e9c6686e452959ef61a971640787fba0e6c6f60c401a1f8b05ada00cdc289ae048c3f99b082028ca1a6f7b7b0eafaf7e6050e4d516b'
